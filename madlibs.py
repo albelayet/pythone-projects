4c887b3cf35a25574962fc2b0018e5549d4c732c
albelayet <albelayet24@gmail.com>
2023-08-25 00:52:21 +0600

    first commit

---
+++ madlibs.py
@@ -0,0 +1,30 @@
+
+
+name ="Jannattttttt"
+
+print("i love you" + name)
+print("i love you {}".format(name))
+print(f"i love you {name}")
+
+name=input('Enter a name: ')
+verb1=input('Enter a verb: ')
+adj=input('Enter a adjective: ')
+verb2=input('Enter a verb: ')
+
+print(f"I love my {name}. I have to {verb1} something. My {adj} also love me \
+so my {verb2} to do something for them")
+
+
+
+                 #HALLOWEEN MAD LIBS
+
+holiday=input("Enter a Holiday: ")
+noun=input("Enter a noun: ")
+place=input("Enter a place: ")
+person=input("Enter a person: ")
+item=input("Enter a item: ")
+
+
+print(f"I can't Believe Its Already {holiday}. I can't wait to put my {noun} and\
+    visit every {place} in my neighborhood. This is year i am going to dress up {person}\
+    Finally all of my {item} ready to go")
